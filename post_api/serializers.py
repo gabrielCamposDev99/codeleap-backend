b44b7fc2b356ab27ca40b73141b66c4ec0e243ed
Gabriel Campos <dev.gabrielscampos@gmail.com>
2024-02-24 13:02:08 -0300

    fix postSerializer name

--- post_api/serializers.py
+++ post_api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from post_api.models import PostModel
 
 
-class NoteSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostModel
         fields = '__all__'
