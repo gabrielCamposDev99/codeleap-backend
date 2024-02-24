5185a680fcb2c12e2c2361a99e8416983ea4249a
Gabriel Campos <dev.gabrielscampos@gmail.com>
2024-02-24 14:02:13 -0300

    add swagger urls

--- posts/urls.py
+++ posts/urls.py
@@ -15,9 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CodeLeap API",
+        default_version='v1',
+        description="Post API built by Gabriel Campos",
+        contact=openapi.Contact(email="dev.gabrielscampos@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', include('post_api.urls'))
+    path('api/posts/', include('post_api.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 ]
