ee8653e1e16e89f157c3b1e17147be01602c05eb
Gabriel Campos <dev.gabrielscampos@gmail.com>
2024-02-24 13:00:05 -0300

    create NoteModel

--- post_api/models.py
+++ post_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class NoteModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    created_datetime = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author_ip = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = "posts"
+
+    def __str__(self):
+        return self.title
