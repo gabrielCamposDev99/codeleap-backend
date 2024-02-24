a77e6f20827edf91942dd4fddc7158345992cd63
Gabriel Campos <dev.gabrielscampos@gmail.com>
2024-02-24 12:45:11 -0300

    config: create env files and cofig postgres connection database

--- posts/settings.py
+++ posts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'posts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'options': f'-c search_path={config("DB_SEARCH_PATH", default="posts")}'
+        }
     }
 }
 
